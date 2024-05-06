ceecbfcc5a943b4bb2a10f35d66901dfd5a488a2
Shampy05 <sharmapranshu317@gmail.com>
2024-05-06 22:53:12 +0100

    added shadows

--- src/script.js
+++ src/script.js
@@ -152,6 +152,7 @@ for (let i = 0; i < 50; i++) {
 
     grave.rotation.y = (Math.random() - 0.5) * 0.4
     grave.rotation.z = (Math.random() - 0.5) * 0.4
+    grave.castShadow = true
 
     graves.add(grave)
 }
@@ -252,6 +253,41 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837')
+
+// shadows
+renderer.shadowMap.enabled = true
+renderer,shadowMap.type = THREE.PCFSoftShadowMap
+
+moonLight.castShadow = true 
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+walls.castShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+
+floor.receiveShadow = true
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
+
 
 /**
  * Animate
@@ -280,7 +316,6 @@ const tick = () =>
 
     // Render
     renderer.render(scene, camera)
-    renderer.setClearColor('#262837')
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
