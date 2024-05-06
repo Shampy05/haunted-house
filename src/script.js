58d92198ba3406862fd71910aa6c160f993d67e2
Shampy05 <sharmapranshu317@gmail.com>
2024-05-06 16:54:12 +0100

    added house components/procedural graves and fog

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { randFloat } from 'three/src/math/MathUtils'
 
 /**
  * Base
@@ -14,21 +15,105 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// helper 
+const axesHelper = new THREE.AxesHelper(10)
+scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
 
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+
+// group 
+const house = new THREE.Group()
+scene.add(house)
+
+//walls 
+const wallHeight = 2.5
+const wallWidth = 4
+const wallDepth = 4
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(wallWidth, wallHeight, wallDepth),
+    new THREE.MeshStandardMaterial({ color: '#ac8e82' })
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = wallHeight / 2
+house.add(walls)
+
+//roof 
+const roofHeight = 2
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, roofHeight, 4),
+    new THREE.MeshStandardMaterial({ color: '#b35f45' })
+)
+house.add(roof)
+roof.position.y = wallHeight + roofHeight / 2
+roof.rotation.y = Math.PI * 0.25
+
+// door 
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 2, 100, 100),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+)
+door.position.z = wallDepth / 2 + 0.01
+door.position.y = 1
+house.add(door)
+
+// bushes 
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+
+// add 3 bushes using loop
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+house.add(bush1)
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.3, 0.3, 0.3)
+bush2.position.set(1.2, 0.2, 2.2)
+house.add(bush2)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.5, 0.5, 0.5)
+bush3.position.set(-0.8, 0.2, 2.2)
+house.add(bush3)
+
+// graves 
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeomtery = new THREE.BoxGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' })
+
+for (let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3 + Math.random() * 6
+    const x = Math.cos(angle) * radius 
+    const z = Math.sin(angle) * radius 
+
+    const grave = new THREE.Mesh(graveGeomtery, graveMaterial)
+
+    grave.position.set(x, 0.3, z)
+
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+
+    graves.add(grave)
+}
+
 
 // Floor
 const floor = new THREE.Mesh(
@@ -43,18 +128,28 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.25)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
+
+
+// door light 
+const doorLight = new THREE.PointLight('#ff7d46', 5, 5)
+doorLight.position.set(0, 2.5, 3.4)
+scene.add(doorLight)
+
+// fog 
+const fog = new THREE.Fog('#262837', 1, 15)
+scene.fog = fog
 
 /**
  * Sizes
@@ -116,6 +211,7 @@ const tick = () =>
 
     // Render
     renderer.render(scene, camera)
+    renderer.setClearColor('#262837')
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
